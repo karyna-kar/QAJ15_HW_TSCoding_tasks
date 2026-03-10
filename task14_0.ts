8416d714d18509e38138d9ad70404c2a3d17b10b
Karina <karyna.karmaza@gmail.com>
2026-03-10 10:02:02 +0200

    run formatting

--- task14_0.ts
+++ task14_0.ts
@@ -1,32 +1,31 @@
 // 0. Реализовать класс калькулятор, с минимум следующими методами: сложение, вычитание, умножение, деление. Эта задача нам пригодится впоследствии
 
-class Calculator{
-  private checkInput(...numbers: number[]){
+class Calculator {
+  private checkInput(...numbers: number[]) {
     if (numbers.length === 0) {
-      throw new Error("No values provided");
+      throw new Error('No values provided');
     }
   }
 
-  add(...numbers: number[]){
+  add(...numbers: number[]) {
     this.checkInput(...numbers);
-    return numbers.reduce((acc, value)=> acc + value);
+    return numbers.reduce((acc, value) => acc + value);
   }
 
-   subtract(...numbers: number[]){
+  subtract(...numbers: number[]) {
     this.checkInput(...numbers);
-    return numbers.reduce((acc, value)=> acc - value);
+    return numbers.reduce((acc, value) => acc - value);
   }
 
-  multiply(...numbers: number[]){
+  multiply(...numbers: number[]) {
     this.checkInput(...numbers);
-    return numbers.reduce((acc, value)=> acc * value);
+    return numbers.reduce((acc, value) => acc * value);
   }
 
-  divide(...numbers: number[]){
+  divide(...numbers: number[]) {
     this.checkInput(...numbers);
     return numbers.reduce((acc, value) => {
-      if (value === 0)
-      {
+      if (value === 0) {
         throw new Error('Dividing by zero is forbidden');
       }
       return acc / value;
--- task14_1.ts
+++ task14_1.ts
@@ -1,9 +1,8 @@
 /*Создайте функцию, которая будет принимать в себя массив значений и возвращать только те, в которых заданное(второй параметр, по умолчанию - 4) количество букв*/
 
 const filterArray = (inputArray: string[], letters: number = 4) => {
-  const result =  inputArray.filter(value => (value.match(/\p{L}/gu) || []).length === letters);
-  if (result.length === 0)
-  {
+  const result = inputArray.filter(value => (value.match(/\p{L}/gu) || []).length === letters);
+  if (result.length === 0) {
     throw new Error('No matches found');
   }
   return result;
