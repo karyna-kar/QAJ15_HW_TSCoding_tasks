203ac06a0fe2ffd059a8a49adcccb904e690021d
Karina <karyna.karmaza@gmail.com>
2026-03-10 09:49:19 +0200

    feat: add solution for task14_1

---
+++ task14_1.ts
@@ -0,0 +1,13 @@
+/*Создайте функцию, которая будет принимать в себя массив значений и возвращать только те, в которых заданное(второй параметр, по умолчанию - 4) количество букв*/
+
+const filterArray = (inputArray: string[], letters: number = 4) => {
+  const result =  inputArray.filter(value => (value.match(/\p{L}/gu) || []).length === letters);
+  if (result.length === 0)
+  {
+    throw new Error('No matches found');
+  }
+  return result;
+};
+
+const input = ['test123', 'example', 'dog', 'x', 'mama'];
+console.log(filterArray(input, 5));
