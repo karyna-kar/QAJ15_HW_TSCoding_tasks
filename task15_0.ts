fe22dc31cb6a5c5ea0a8af171df1d383cf9b4f7c
Karina <karyna.karmaza@gmail.com>
2026-03-11 19:05:44 +0200

    run formatting

--- task15_0.ts
+++ task15_0.ts
@@ -8,7 +8,7 @@
 //     { name: 'C', price: 10, quantity: 1, inStock: true },
 //     { name: 'D', price: 200, quantity: 3, inStock: false }
 // ];
-const cost = 500; 
+const cost = 500;
 
 type CartItem = {
   name: string;
@@ -18,16 +18,16 @@ type CartItem = {
 };
 
 const calculateCartTotal = (cart: CartItem[]) => {
-  const filteredCart = cart.filter(item => item.inStock || (item.price * item.quantity) > cost);
+  const filteredCart = cart.filter(item => item.inStock || item.price * item.quantity > cost);
   const total = filteredCart.reduce((sum, item) => sum + item.price * item.quantity, 0);
   return total;
 };
 
 const products = [
-    { name: 'A', price: 100, quantity: 2, inStock: true },
-     { name: 'B', price: 40, quantity: 5, inStock: false },
-    { name: 'C', price: 10, quantity: 1, inStock: true },
-    { name: 'D', price: 200, quantity: 3, inStock: false }
- ];
+  { name: 'A', price: 100, quantity: 2, inStock: true },
+  { name: 'B', price: 40, quantity: 5, inStock: false },
+  { name: 'C', price: 10, quantity: 1, inStock: true },
+  { name: 'D', price: 200, quantity: 3, inStock: false }
+];
 
 console.log(`TotalCartValue = ${calculateCartTotal(products)}`);
