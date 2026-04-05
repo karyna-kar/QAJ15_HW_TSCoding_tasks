eed0e3f94ead371fcafe550eca26f3d15d4642f0
Karina <karyna.karmaza@gmail.com>
2026-04-05 13:06:54 +0300

    Add helpers

---
+++ mocha/helper/objects-client.ts
@@ -0,0 +1,144 @@
+import type { PostObjectPayload } from './types';
+
+export class ObjectClient {
+  private baseUrl = 'https://api.restful-api.dev/collections/products/objects';
+  private key = '32382330-1bba-4fc7-b146-407a38d7443d';
+
+  async getObjects(ids?: string[]) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-api-key': this.key
+      }
+    };
+
+    if (ids && ids.length > 0) {
+      const params = new URLSearchParams({ id: ids.join(',') });
+      const response = await fetch(`${this.baseUrl}?${params}`, options);
+      const body = await response.json();
+
+      return {
+        status: response.status,
+        body
+      };
+    } else {
+      const response = await fetch(this.baseUrl, options);
+      const body = await response.json();
+
+      return {
+        status: response.status,
+        body
+      };
+    }
+  }
+
+  async getObjectById(id: string) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-api-key': this.key
+      }
+    };
+
+    const response = await fetch(`${this.baseUrl}/${id}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+
+  async createObject(objectPayload: PostObjectPayload) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': this.key
+      },
+      body: JSON.stringify(objectPayload)
+    };
+
+    const response = await fetch(`${this.baseUrl}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+
+  async createInvalidObject(objectPayload: {}) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': this.key
+      },
+      body: JSON.stringify(objectPayload)
+    };
+
+    const response = await fetch(`${this.baseUrl}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+
+  async fullyUpdateObject(objectPayload: PostObjectPayload, id: string) {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': this.key
+      },
+      body: JSON.stringify(objectPayload)
+    };
+
+    const response = await fetch(`${this.baseUrl}/${id}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+
+  async partialUpdateObject(objectPayload: {}, id: string) {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': this.key
+      },
+      body: JSON.stringify(objectPayload)
+    };
+
+    const response = await fetch(`${this.baseUrl}/${id}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+
+  async deleteObjectById(id: string) {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'x-api-key': this.key
+      }
+    };
+
+    const response = await fetch(`${this.baseUrl}/${id}`, options);
+    const body = await response.json();
+
+    return {
+      status: response.status,
+      body
+    };
+  }
+}
