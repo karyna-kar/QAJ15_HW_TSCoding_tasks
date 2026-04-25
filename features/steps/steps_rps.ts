533995920b4f116161c3e496fa9b5839c138152c
Karina <karyna.karmaza@gmail.com>
2026-04-25 17:08:44 +0300

    add test scenatios and steps

---
+++ features/steps/steps_rps.ts
@@ -0,0 +1,21 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+import { Move, RockPaperScissors } from '../../rps';
+import { expect } from 'chai';
+
+type TestContex = {
+  game: RockPaperScissors;
+  result: string;
+  generatedMove: Move;
+};
+
+Given("There's a player {string}", function (name: string) {
+  return (this.game = new RockPaperScissors(name));
+});
+
+When('Player choose {string} and bot choose {string}', function (this: TestContex, playerMove: Move, botMove: Move) {
+  return (this.result = this.game.determineWinner(playerMove, botMove));
+});
+
+Then('Game result is {string}', function (this: TestContex, expectedResult: String) {
+  expect(this.result).to.equal(expectedResult);
+});
